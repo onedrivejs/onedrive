be52d658749afc752dec736ca499e84267d537a0
David Barratt <david@davidwbarratt.com>
2018-07-08 13:09:43 -0400

    Get code coverage to 100%

--- src/fs/stream.test.js
+++ src/fs/stream.test.js
@@ -1,9 +1,11 @@
 const EventEmitter = require('events');
 const chokidar = require('chokidar');
+const hasha = require('hasha');
 const { take } = require('rxjs/operators');
 const createStream = require('./stream');
 
 jest.mock('chokidar');
+jest.mock('hasha');
 
 const watcher = new EventEmitter();
 watcher.options = {
@@ -11,6 +13,7 @@ watcher.options = {
 };
 
 chokidar.watch = jest.fn(() => watcher);
+hasha.fromFile = jest.fn(() => Promise.resolve('abcdef'));
 
 test('creates a filesystem stream', () => {
   const stream = createStream('data');
@@ -27,5 +30,17 @@ test('sends a test event through the stream', () => {
   return expect(data).resolves.toEqual({
     event: 'test',
     path: 'what/what.jpg',
+  });
+});
+
+test('sends an add event through the stream', () => {
+  const data = createStream('data').pipe(take(1)).toPromise();
+
+  watcher.emit('all', 'add', 'what/what.jpg');
+
+  return expect(data).resolves.toEqual({
+    event: 'add',
+    path: 'what/what.jpg',
+    hash: 'abcdef',
   });
 });
