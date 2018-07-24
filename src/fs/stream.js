4ed5db752cb4eec286f2c1840dc1cda8c5cdfac8
David Barratt <david@davidwbarratt.com>
2018-07-24 17:18:40 -0400

    Normalize events into a stream

--- src/fs/stream.js
+++ src/fs/stream.js
@@ -1,120 +1,161 @@
-const path = require('path');
 const { Client } = require('fb-watchman');
-const {
-  fromEvent,
-  from,
-  of,
-  merge,
-} = require('rxjs');
-const {
-  filter,
-  flatMap,
-  delay,
-  tap,
-  map,
-  takeUntil,
-} = require('rxjs/operators');
+const { fromEvent, from } = require('rxjs');
+const { flatMap, map, filter } = require('rxjs/operators');
+const { Map, Set } = require('immutable');
 
 const client = new Client();
 
+const formatAction = (action, file) => {
+  let type;
+
+  // @TODO split into a seperate function.
+  switch (file.type) {
+    case 'f':
+      type = 'file';
+      break;
+    case 'd':
+      type = 'folder';
+      break;
+    default:
+      break;
+  }
+
+  // @TODO Add the lastModifiedDateTime to the action
+  return {
+    action,
+    id: file.ino,
+    type,
+    name: file.name,
+    hash: typeof file['content.sha1hex'] === 'string' ? file['content.sha1hex'] : null,
+  };
+};
+
 const stream = (directory) => {
+  let adding = new Set();
+  let hashes = new Map();
+
   client.command(['subscribe', directory, 'onedrive', {
     expression: ['not', ['match', '.*']],
-    fields: ['name', 'ino', 'content.sha1hex', 'type', 'exists', 'new'],
-  }], (error) => {
-    if (error) {
-      // Probably an error in the subscription criteria
-      console.error('failed to subscribe: ', error);
-    }
-  });
+    fields: ['name', 'ino', 'content.sha1hex', 'type', 'exists', 'new', 'mtime_ms'],
+  }]);
+
+  // Debug
+  // return fromEvent(client, 'subscription');
 
   // Normalize filesystem events.
   return fromEvent(client, 'subscription').pipe(
     flatMap((resp) => {
-      return of(resp);
-    }),
-    // flatMap(([event, p]) => {
-    //   switch (event) {
-    //     case 'add':
-    //     case 'change':
-    //       return from(hasha.fromFile(path.resolve(cwd, p), { algorithm: 'sha1' })).pipe(
-    //         map(h => ({
-    //           event,
-    //           path: p,
-    //           hash: h,
-    //         })),
-    //       );
-    //     case 'unlink':
-    //       return of({
-    //         event: 'delete',
-    //         path: p,
-    //         hash: store.get(p),
-    //       });
-    //     default:
-    //       return of({
-    //         event,
-    //         path: p,
-    //       });
-    //   }
-    // }),
-  );
+      // Create a map of files by id.
+      const fileActions = resp.files.reduce((map, file) => {
+        const a = [
+          ...map.get(file.ino, []),
+          file,
+        ];
 
-  // Merge add & delete action into move.
-  // return merge(fs, move).pipe(
-  //   flatMap((data) => {
-  //     switch (data.event) {
-  //       case 'change':
-  //         store = store.set(data.path, data.hash);
-  //         return of(data);
-  //       // @TODO Use groupByUntil operator instead of this nonsense.
-  //       case 'add':
-  //         return of(data).pipe(
-  //           flatMap((h) => {
-  //             // If this is a change, or if the hash does not exist in the file
-  //             // system, send the even immdiatly.
-  //             if (!store.find(fileHash => fileHash === data.hash)) {
-  //               store = store.set(data.path, data.hash);
-  //               return of(data);
-  //             }
-  //
-  //             // Notify any waiting unlinks.
-  //             add.next(data);
-  //
-  //             // If the hash is in the store, it might be a move.
-  //             return of(data).pipe(
-  //               // Give the filesystem a moment to relink the file.
-  //               delay(100),
-  //               takeUntil(move.pipe(
-  //                 filter(moveData => moveData.hash === h),
-  //               )),
-  //             );
-  //           }),
-  //         );
-  //       case 'delete':
-  //         return of(data).pipe(
-  //           // Give the filesystem a moment to relink the file.
-  //           delay(100),
-  //           takeUntil(add.pipe(
-  //             filter(addData => addData.hash === data.hash),
-  //             tap((addData) => {
-  //               store = store.delete(data.path);
-  //
-  //               move.next({
-  //                 ...addData,
-  //                 event: 'move',
-  //                 oldPath: data.path,
-  //               });
-  //             }),
-  //           )),
-  //           tap((d) => {
-  //             store = store.delete(d.path);
-  //           }),
-  //         );
-  //       default:
-  //         return of(data);
-  //     }
-  //   }),
-  // );
+        return map.set(file.ino, a);
+      }, new Map());
+
+      return from(fileActions.map((items) => {
+        let action;
+
+        // Resolve two actions.
+        if (items.length === 2) {
+          const [first, second] = items;
+
+          if (!first.exists
+            && !first.new
+            && second.exists
+            && second.new
+          ) {
+            return {
+              ...formatAction('move', second),
+              oldName: first.name,
+            };
+          }
+        }
+
+        // Something is wrong.
+        if (items.length > 2) {
+          console.dir(items);
+          throw new Error('Unhandled watchman action');
+        }
+
+        // Only a single action.
+        const [file] = items;
+
+        if (file.exists) {
+          if (file.new) {
+            action = 'add';
+          } else {
+            action = 'change';
+          }
+        } else {
+          action = 'remove';
+        }
+
+        return formatAction(action, file);
+      }).toArray());
+    }),
+    // Hold the add actions until a hash (from a change) is present.
+    map((value) => {
+      if (value.type === 'file') {
+        if (value.action === 'add' && value.hash === null) {
+          // Add to state store.
+          adding = adding.add(value.id);
+          return undefined;
+        }
+
+        if (value.action === 'change' && value.hash === null) {
+          // File is still writting.
+          return undefined;
+        }
+
+        if (
+          value.action === 'change'
+          && value.hash !== null
+          && adding.get(value.id)
+        ) {
+          // Remove from state.
+          adding = adding.remove(value.id);
+          return {
+            ...value,
+            action: 'add',
+          };
+        }
+      }
+
+      return value;
+    }),
+    // Remove all the undefined values.
+    filter(v => !!v),
+    map((value) => {
+      if (value.action === 'add' && value.hash) {
+        const name = hashes.findKey(hash => hash === value.hash);
+        if (name) {
+          hashes = hashes.set(value.name, value.hash);
+          return {
+            ...value,
+            action: 'copy',
+            from: name,
+          };
+        }
+      }
+
+      // When a file is deleted, remove the hash from the state.
+      if (value.action === 'remove') {
+        hashes = hashes.remove(value.name);
+        return value;
+      }
+
+      // Add every hash to the store (if there is one)
+      if (value.hash) {
+        hashes = hashes.set(value.name, value.hash);
+        return value;
+      }
+
+      return value;
+    }),
+  );
 };
 
 module.exports = stream;
--- src/fs/stream.test.js
+++ src/fs/stream.test.js
@@ -21,79 +21,236 @@ test('creates a filesystem stream', () => {
   expect(stream).toBeDefined();
 });
 
-test('sends a test event through the stream', () => {
-  const client = new Client();
-  const data = createStream('data').pipe(take(1)).toPromise();
-
-  client.emit('subscription', 'test', '');
-  client.emit('subscription', 'test', 'what/what.jpg');
-
-  return expect(data).resolves.toEqual({
-    event: 'test',
-    path: 'what/what.jpg',
-  });
-});
-
 test('sends an add event through the stream', () => {
   const client = new Client();
-  const data = createStream('data').pipe(take(1)).toPromise();
+  const stream = createStream('data');
 
-  client.emit('subscription', 'add', 'what/what.jpg');
+  const data = Promise.all([
+    stream.pipe(take(1)).toPromise(),
+    stream.pipe(take(2)).toPromise(),
+    stream.pipe(take(3)).toPromise(),
+  ]);
 
-  return expect(data).resolves.toEqual({
-    event: 'add',
-    path: 'what/what.jpg',
-    hash: 'abcdef',
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test',
+        ino: 123,
+        'content.sha1hex': null,
+        type: 'd',
+        exists: true,
+        new: true,
+      },
+      {
+        name: 'test/test.jpg',
+        ino: 321,
+        'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+        type: 'f',
+        exists: true,
+        new: true,
+      },
+      {
+        name: 'test/test2.jpg',
+        ino: 567,
+        'content.sha1hex': {
+          error: 'changed'
+        },
+        type: 'f',
+        exists: true,
+        new: true,
+      },
+    ],
   });
+
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test2.jpg',
+        ino: 567,
+        'content.sha1hex': {
+          error: 'changed',
+        },
+        type: 'f',
+        exists: true,
+        new: false,
+      },
+    ],
+  });
+
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test2.jpg',
+        ino: 567,
+        'content.sha1hex': 'd204',
+        type: 'f',
+        exists: true,
+        new: false,
+      },
+    ],
+  });
+
+  return expect(data).resolves.toEqual([
+    {
+      action: 'add',
+      id: 123,
+      type: 'folder',
+      name: 'test',
+      hash: null,
+    },
+    {
+      action: 'add',
+      id: 321,
+      type: 'file',
+      name: 'test/test.jpg',
+      hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+    },
+    {
+      action: 'add',
+      id: 567,
+      type: 'file',
+      name: 'test/test2.jpg',
+      hash: 'd204',
+    },
+  ]);
 });
 
+//
 test('sends a change event through the stream', () => {
   const client = new Client();
-  const data = createStream('data').pipe(take(1)).toPromise();
+  const stream = createStream('data');
+  const data = stream.pipe(take(1)).toPromise();
 
-  client.emit('subscription', 'change', 'what/what.jpg');
-
-  return expect(data).resolves.toEqual({
-    event: 'change',
-    path: 'what/what.jpg',
-    hash: 'abcdef',
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test.txt',
+        ino: 321,
+        'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+        type: 'f',
+        exists: true,
+        new: false,
+      },
+    ],
   });
+
+  return expect(data).resolves.toEqual(
+    {
+      action: 'change',
+      id: 321,
+      type: 'file',
+      name: 'test/test.txt',
+      hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+    },
+  );
+});
+
+test('sends a remove event through the stream', () => {
+  const client = new Client();
+  const stream = createStream('data');
+  const data = stream.pipe(take(1)).toPromise();
+
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test.txt',
+        ino: 321,
+        'content.sha1hex': null,
+        type: 'f',
+        exists: false,
+        new: false,
+      },
+    ],
+  });
+
+  return expect(data).resolves.toEqual(
+    {
+      action: 'remove',
+      id: 321,
+      type: 'file',
+      name: 'test/test.txt',
+      hash: null,
+    },
+  );
 });
 
 test('sends a move event through the stream', () => {
   const client = new Client();
-  const data = createStream('data').pipe(take(2)).toPromise();
+  const stream = createStream('data');
+  const data = stream.pipe(take(1)).toPromise();
 
-  // Init.
-  client.emit('subscription', 'add', 'what/what.jpg');
-  // At somepoint in the future.
-  setTimeout(() => {
-    client.emit('subscription', 'unlink', 'what/what.jpg');
-    client.emit('subscription', 'add', 'what/what2.jpg');
-  }, 200);
-
-  return expect(data).resolves.toEqual({
-    event: 'move',
-    path: 'what/what2.jpg',
-    oldPath: 'what/what.jpg',
-    hash: 'abcdef',
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test.txt',
+        ino: 321,
+        'content.sha1hex': null,
+        type: 'f',
+        exists: false,
+        new: false,
+      },
+      {
+        name: 'test/test2.txt',
+        ino: 321,
+        'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+        type: 'f',
+        exists: true,
+        new: true,
+      },
+    ],
   });
+
+  return expect(data).resolves.toEqual(
+    {
+      action: 'move',
+      id: 321,
+      type: 'file',
+      name: 'test/test2.txt',
+      hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+      oldName: 'test/test.txt',
+    },
+  );
 });
 
-test('sends a delete event through the stream', () => {
+test('sends a copy event through the stream', () => {
   const client = new Client();
-  const data = createStream('data').pipe(take(2)).toPromise();
+  const stream = createStream('data');
+  const data = stream.pipe(take(2)).toPromise();
 
-  // Init.
-  client.emit('subscription', 'add', 'what/what.jpg');
-  // At somepoint in the future.
-  setTimeout(() => {
-    client.emit('subscription', 'unlink', 'what/what.jpg');
-  }, 200);
-
-  return expect(data).resolves.toEqual({
-    event: 'delete',
-    path: 'what/what.jpg',
-    hash: 'abcdef',
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test.txt',
+        ino: 123,
+        'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+        type: 'f',
+        exists: true,
+        new: true,
+      },
+    ],
   });
+
+  client.emit('subscription', {
+    files: [
+      {
+        name: 'test/test2.txt',
+        ino: 321,
+        'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+        type: 'f',
+        exists: true,
+        new: true,
+      },
+    ],
+  });
+
+  return expect(data).resolves.toEqual(
+    {
+      action: 'copy',
+      id: 321,
+      type: 'file',
+      name: 'test/test2.txt',
+      hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+      from: 'test/test.txt',
+    },
+  );
 });
