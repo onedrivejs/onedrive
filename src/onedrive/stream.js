0ad4df444d1ff6856bd2ed68082d4cb1729fd7ed
David Barratt <david@davidwbarratt.com>
2018-08-03 17:05:40 -0400

    Handle additional OneDrive actions

--- src/onedrive/stream.js
+++ src/onedrive/stream.js
@@ -1,10 +1,13 @@
 const { map } = require('rxjs/operators');
 const { DateTime } = require('luxon');
 const { join } = require('path');
+const { Map } = require('immutable');
 const delta = require('./delta');
 
-const stream = refreshToken => (
-  delta(refreshToken).pipe(
+const stream = (refreshToken) => {
+  let files = new Map();
+
+  return delta(refreshToken).pipe(
     map((file) => {
       let type;
       if ('file' in file) {
@@ -18,18 +21,61 @@ const stream = refreshToken => (
       // Debug
       // return file;
 
-      // @TODO Deal with change/move/copy/delete.
-      return {
-        action: 'add',
+      const hash = file.file && file.file.hashes ? file.file.hashes.sha1Hash : null;
+      const name = join(file.parentReference.path, file.name).replace('/drive/root:/', '');
+
+      let action;
+      let oldName;
+      let from;
+
+      const basic = {
         id: file.id,
         type,
-        name: join(file.parentReference.path, file.name).replace('/drive/root:/', ''),
+        name,
+      };
+
+      if ('deleted' in file) {
+        files = files.remove(file.id);
+        return {
+          action: 'remove',
+          ...basic,
+        };
+      }
+
+      const existing = files.get(file.id);
+      if (existing) {
+        if (existing.name !== name) {
+          action = 'move';
+          oldName = existing.name;
+        } else {
+          action = 'change';
+        }
+      } else {
+        const other = files.find(f => f.hash === hash);
+        if (other) {
+          action = 'copy';
+          from = other.name;
+        } else {
+          action = 'add';
+        }
+      }
+
+      files = files.set(file.id, {
+        name,
+        hash,
+      });
+
+      return {
+        action,
+        ...basic,
+        from,
         modified: file.lastModifiedDateTime ? DateTime.fromISO(file.lastModifiedDateTime) : null,
-        hash: file.file ? file.file.hashes.sha1Hash : null,
+        hash,
         downloadUrl: file['@microsoft.graph.downloadUrl'] ? file['@microsoft.graph.downloadUrl'] : null,
+        oldName,
       };
     }),
-  )
-);
+  );
+};
 
 module.exports = stream;
--- src/onedrive/stream.test.js
+++ src/onedrive/stream.test.js
@@ -85,230 +85,156 @@ test('unhandled item type', () => {
 
   return expectation;
 });
-//
-//
-// test('change event', () => {
-//   const client = new Client();
-//   const stream = createStream(client, 'data');
-//   const data = stream.pipe(take(1)).toPromise();
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test.txt',
-//         ino: 321,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'f',
-//         exists: true,
-//         new: false,
-//       },
-//     ],
-//   });
-//
-//   return expect(data).resolves.toEqual(
-//     {
-//       action: 'change',
-//       id: 321,
-//       modified: null,
-//       type: 'file',
-//       name: 'test/test.txt',
-//       hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//     },
-//   );
-// });
-//
-// test('remove event', () => {
-//   const client = new Client();
-//   const stream = createStream(client, 'data');
-//   const data = stream.pipe(take(1)).toPromise();
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test.txt',
-//         ino: 321,
-//         modified: null,
-//         'content.sha1hex': null,
-//         type: 'f',
-//         exists: false,
-//         new: false,
-//       },
-//     ],
-//   });
-//
-//   return expect(data).resolves.toEqual(
-//     {
-//       action: 'remove',
-//       id: 321,
-//       modified: null,
-//       type: 'file',
-//       name: 'test/test.txt',
-//       hash: null,
-//     },
-//   );
-// });
-//
-// test('move event', () => {
-//   const client = new Client();
-//   const stream = createStream(client, 'data');
-//   const data = stream.pipe(take(1)).toPromise();
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test.txt',
-//         ino: 321,
-//         'content.sha1hex': null,
-//         type: 'f',
-//         exists: false,
-//         new: false,
-//       },
-//       {
-//         name: 'test/test2.txt',
-//         ino: 321,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'f',
-//         exists: true,
-//         new: true,
-//       },
-//     ],
-//   });
-//
-//   return expect(data).resolves.toEqual(
-//     {
-//       action: 'move',
-//       id: 321,
-//       modified: null,
-//       type: 'file',
-//       name: 'test/test2.txt',
-//       hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//       oldName: 'test/test.txt',
-//     },
-//   );
-// });
-//
-// test('copy event', () => {
-//   const client = new Client();
-//   const stream = createStream(client, 'data');
-//   const data = stream.pipe(take(2)).toPromise();
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test.txt',
-//         ino: 123,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'f',
-//         exists: true,
-//         new: true,
-//       },
-//     ],
-//   });
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test2.txt',
-//         ino: 321,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'f',
-//         exists: true,
-//         new: true,
-//       },
-//     ],
-//   });
-//
-//   return expect(data).resolves.toEqual(
-//     {
-//       action: 'copy',
-//       id: 321,
-//       modified: null,
-//       type: 'file',
-//       name: 'test/test2.txt',
-//       hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//       from: 'test/test.txt',
-//     },
-//   );
-// });
-//
-// test('bogus file type', () => {
-//   const client = new Client();
-//   const stream = createStream(client, 'data');
-//   const data = stream.pipe(take(1)).toPromise();
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test.txt',
-//         ino: 321,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'l',
-//         exists: true,
-//         new: false,
-//       },
-//     ],
-//   });
-//
-//   return expect(data).resolves.toEqual(
-//     {
-//       action: 'change',
-//       id: 321,
-//       modified: null,
-//       type: '',
-//       name: 'test/test.txt',
-//       hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//     },
-//   );
-// });
-//
-// test('two changes to the same file', () => {
-//   const client = new Client();
-//   const stream = createStream(client, 'data');
-//   const data = Promise.all([
-//     stream.pipe(take(1)).toPromise(),
-//     stream.pipe(take(2)).toPromise(),
-//   ]);
-//
-//   client.emit('subscription', {
-//     files: [
-//       {
-//         name: 'test/test.txt',
-//         ino: 321,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'f',
-//         exists: true,
-//         new: false,
-//       },
-//       {
-//         name: 'test/test.txt',
-//         ino: 321,
-//         'content.sha1hex': 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//         type: 'f',
-//         exists: true,
-//         new: false,
-//       },
-//     ],
-//   });
-//
-//   return expect(data).resolves.toEqual([
-//     {
-//       action: 'change',
-//       id: 321,
-//       modified: null,
-//       type: 'file',
-//       name: 'test/test.txt',
-//       hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//     },
-//     {
-//       action: 'change',
-//       id: 321,
-//       modified: null,
-//       type: 'file',
-//       name: 'test/test.txt',
-//       hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
-//     },
-//   ]);
-// });
+
+
+test('change event', () => {
+  const subject = new Subject();
+  delta.mockReturnValue(subject);
+  const stream = createStream('1234');
+
+  const data = stream.pipe(take(2)).toPromise();
+
+  const item = {
+    id: '321',
+    name: 'test.jpg',
+    file: {
+      hashes: {
+        sha1Hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+      },
+    },
+    parentReference: {
+      path: '/drive/root:/test',
+    },
+    '@microsoft.graph.downloadUrl': 'https://example.com',
+  };
+
+  subject.next(item);
+  subject.next({
+    ...item,
+    file: {
+      ...item.file,
+      hashes: {
+        ...item.file.hashes,
+        sha1Hash: 'abcdef',
+      },
+    },
+  });
+
+  return expect(data).resolves.toEqual({
+    action: 'change',
+    id: '321',
+    modified: null,
+    type: 'file',
+    name: 'test/test.jpg',
+    hash: 'abcdef',
+    downloadUrl: 'https://example.com',
+  });
+});
+
+test('remove event', () => {
+  const subject = new Subject();
+  delta.mockReturnValue(subject);
+  const stream = createStream('1234');
+
+  const data = stream.pipe(take(1)).toPromise();
+
+  subject.next({
+    id: '321',
+    name: 'test.jpg',
+    file: {},
+    deleted: {},
+    parentReference: {
+      path: '/drive/root:/test',
+    },
+    '@microsoft.graph.downloadUrl': 'https://example.com',
+  });
+
+  return expect(data).resolves.toEqual({
+    action: 'remove',
+    id: '321',
+    type: 'file',
+    name: 'test/test.jpg',
+  });
+});
+
+test('move event', () => {
+  const subject = new Subject();
+  delta.mockReturnValue(subject);
+  const stream = createStream('1234');
+
+  const data = stream.pipe(take(2)).toPromise();
+
+  const item = {
+    id: '321',
+    name: 'test.jpg',
+    file: {
+      hashes: {
+        sha1Hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+      },
+    },
+    parentReference: {
+      path: '/drive/root:/test',
+    },
+    '@microsoft.graph.downloadUrl': 'https://example.com',
+  };
+
+  subject.next(item);
+  subject.next({
+    ...item,
+    name: 'test2.jpg',
+  });
+
+  return expect(data).resolves.toEqual({
+    action: 'move',
+    id: '321',
+    modified: null,
+    type: 'file',
+    name: 'test/test2.jpg',
+    hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+    downloadUrl: 'https://example.com',
+    oldName: 'test/test.jpg',
+  });
+});
+
+test('copy event', () => {
+  const subject = new Subject();
+  delta.mockReturnValue(subject);
+  const stream = createStream('1234');
+
+  const data = stream.pipe(take(2)).toPromise();
+
+  const item = {
+    id: '321',
+    name: 'test.jpg',
+    file: {
+      hashes: {
+        sha1Hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+      },
+    },
+    parentReference: {
+      path: '/drive/root:/test',
+    },
+    '@microsoft.graph.downloadUrl': 'https://example.com',
+  };
+
+  subject.next(item);
+  subject.next({
+    ...item,
+    name: 'test2.jpg',
+    id: '123',
+  });
+
+  return expect(data).resolves.toEqual({
+    action: 'copy',
+    id: '123',
+    modified: null,
+    type: 'file',
+    name: 'test/test2.jpg',
+    from: 'test/test.jpg',
+    hash: 'd2047600b00eec51bf0dcf99c0bc7a77cc76152f',
+    downloadUrl: 'https://example.com',
+  });
+});
 
 test('modified time returns datetime object', () => {
   const subject = new Subject();
