eeaba92d744fb3257e5e2f0dcf4801f83a26ee99
David Barratt <david@davidwbarratt.com>
2018-08-05 15:35:43 -0400

    Increase coverage of create

--- src/fs/resolver/create.js
+++ src/fs/resolver/create.js
@@ -1,21 +1,12 @@
-const { join, dirname } = require('path');
+const { join } = require('path');
 const { ensureDir } = require('fs-extra');
 const formatAction = require('./format');
 
 const createFolder = async (directory, name) => {
   const path = join(directory, name);
   const type = 'folder';
-  try {
-    await ensureDir(dirname(path));
-    return formatAction('create', 'end', type, name);
-  } catch (e) {
-    if (e.code === 'EEXIST') {
-      return formatAction('create', 'exists', type, name);
-    }
-
-    // Some other error we don't know how to deal with.
-    throw e;
-  }
+  await ensureDir(path);
+  return formatAction('create', 'end', type, name);
 };
 
 module.exports = createFolder;
