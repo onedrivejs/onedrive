55680c9b6c30cb9b3dbd7681c0c048dc2c6958fd
David Barratt <david@davidwbarratt.com>
2018-08-18 18:41:42 -0400

    Add ability to create folders

--- src/onedrive/resolver/create.js
+++ src/onedrive/resolver/create.js
@@ -1,34 +1,36 @@
-const { basename } = require('path');
+const { sep } = require('path');
 const createFetch = require('../fetch');
 const formatAction = require('../../utils/format-action');
 
 const createFolder = async (refreshToken, name) => {
-  // @TODO The documentation implies that you can provide the path,
-  //       but that doesn't seem to be the case. We'll have to recurisvely
-  //       create folders until we get to where we want to be.
-  const base = basename(name);
-  let parent = '';
-  if (base.length !== name.length) {
-    parent = `/${name.substring(0, name.length - base.length - 1)}`;
-  }
+  let parentId = 'root';
   const fetch = await createFetch(refreshToken);
-  const url = `https://graph.microsoft.com/v1.0/me/drive/items/root${parent}/children`;
-  const response = await fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      name,
-      folder: {},
-    }),
-  });
+  const parts = name.split(sep);
 
-  const data = await response.json();
-  if (!response.ok) {
-    const error = new Error(`${response.status} ${response.statusText} ${url}`);
-    error.data = data;
-    return formatAction('create', error, 'folder', name);
+  // Itentially adding a loop so the requests are *not* executed in parallel.
+  // eslint-disable-next-line no-restricted-syntax
+  for (const folderName of parts) {
+    const url = `https://graph.microsoft.com/v1.0/me/drive/items/${parentId}/children`;
+    // eslint-disable-next-line no-await-in-loop
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: folderName,
+        folder: {},
+      }),
+    });
+
+    const data = await response.json(); // eslint-disable-line no-await-in-loop
+    if (!response.ok) {
+      const error = new Error(`${response.status} ${response.statusText} ${url}`);
+      error.data = data;
+      return formatAction('create', error, 'folder', name);
+    }
+
+    parentId = data.id;
   }
 
   return formatAction('create', 'end', 'folder', name);
