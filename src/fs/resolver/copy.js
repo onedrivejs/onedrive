d77abf31f118219d0cd594145bc908a31ebe1ed3
David Barratt <david@davidwbarratt.com>
2018-08-25 22:15:50 -0400

    Remove TODO

--- src/fs/resolver/copy.js
+++ src/fs/resolver/copy.js
@@ -6,16 +6,6 @@ const {
 } = require('fs-extra');
 const { formatAction } = require('../../utils/format-action');
 
-// @TODO Create some sort of method for preventing a useless copy. A useless
-//       copy is when the file you are copying from has the same hash as the
-//       file you are copying too. Basically, if you are overridding a file with
-//       the copy, check to ensure that it's not pointless. maybe the shouldCopyFile
-//       can return an ENUM? Or maybe it would be best if it just returned the
-//       proper observable or an empty one. This could also be moved to a
-//       a seperate file since it deals with both copying and downloading.
-//       maybe rename to copyDownloadFile and it's just a new observable with a
-//       flatMap.
-
 const copyFile = (directory, name, fromName) => {
   const type = 'file';
   const path = join(directory, name);
