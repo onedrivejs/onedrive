8ab203333f67081ad901ac1c956e920c632bbab1
David Barratt <david@davidwbarratt.com>
2018-11-03 20:52:16 -0400

    Download a file into the temp directory before moving it to watch directory

--- src/fs/resolver/download.js
+++ src/fs/resolver/download.js
@@ -1,16 +1,19 @@
-const { join, dirname } = require('path');
+const { join, dirname, extname } = require('path');
+const { tmpdir } = require('os');
 const { from, merge, EMPTY } = require('rxjs');
 const { flatMap } = require('rxjs/operators');
 const { fromFile: hashFromFile } = require('hasha');
-const { ensureDir, copy } = require('fs-extra');
+const { ensureDir, move, copy } = require('fs-extra');
 const fs = require('fs');
 const { DateTime } = require('luxon');
 const { promisify } = require('util');
 const promisePipe = require('promisepipe');
 const { PassThrough } = require('stream');
+const { monotonicFactory } = require('ulid');
 const createError = require('../../utils/error');
 const { formatAction } = require('../../utils/format-action');
 
+const ulid = monotonicFactory();
 const stat = promisify(fs.stat);
 const utimes = promisify(fs.utimes);
 
@@ -63,14 +66,17 @@ const downloadFile = (directory, name, hash, modified, downloader) => {
             return formatAction('download', createError(response), type, name);
           }
 
+          const tmpPath = join(tmpdir(), ulid() + extname(path));
+          const body = new PassThrough();
+          response.body.pipe(body);
+          await promisePipe(body, fs.createWriteStream(tmpPath, {
+            flags: 'wx',
+          }));
+          await utimes(tmpPath, new Date(), modified.toJSDate());
+
           try {
             await ensureDir(dirname(path));
-            const body = new PassThrough();
-            response.body.pipe(body);
-            await promisePipe(body, fs.createWriteStream(path, {
-              flags: 'wx',
-            }));
-            await utimes(path, new Date(), modified.toJSDate());
+            await move(tmpPath, path);
             return formatAction('download', 'end', type, name);
           } catch (error) {
             // If the file was going to be overridden.
@@ -78,13 +84,15 @@ const downloadFile = (directory, name, hash, modified, downloader) => {
               // Copy the existing file to the trash.
               const trashPath = join(directory, '.trash', name);
               await ensureDir(dirname(trashPath));
-              await copy(path, trashPath);
+              await move(tmpPath, path, {
+                overwrite: true,
+              });
 
-              const body = new PassThrough();
-              response.body.pipe(body);
               // Allow override this time.
-              await promisePipe(body, fs.createWriteStream(path));
-              await utimes(path, new Date(), modified.toJSDate());
+              await ensureDir(dirname(path));
+              await copy(tmpPath, path, {
+                preserveTimestamps: true,
+              });
               return formatAction('download', 'end', type, name);
             }
 
