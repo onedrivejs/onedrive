b245891c2ba7069fe940266f0614b4300169bd27
David Barratt <david@davidwbarratt.com>
2018-11-10 20:49:49 -0500

    Slow down actions to not overwhelm network

--- src/onedrive/resolver/move.js
+++ src/onedrive/resolver/move.js
@@ -1,39 +1,48 @@
-const { merge } = require('rxjs');
+const { of, merge } = require('rxjs');
+const { flatMap } = require('rxjs/operators');
 const { basename } = require('path');
 const createFetch = require('../fetch');
 const getParent = require('./parent');
 const createError = require('../../utils/error');
 const { formatAction } = require('../../utils/format-action');
+const createSeparator = require('../../separator');
+
+const separator = createSeparator();
 
 const moveItem = (refreshToken, type, name, { id, parentReference: { driveId } }) => (
-  merge(
-    formatAction('move', 'start', type, name),
-    Promise.resolve().then(async () => {
-      const fetch = await createFetch(refreshToken);
-      const { id: parentId, driveId: parentDriveId } = await getParent(fetch, name);
+  of(undefined).pipe(
+    separator,
+    flatMap(() => (
+      merge(
+        formatAction('move', 'start', type, name),
+        Promise.resolve().then(async () => {
+          const fetch = await createFetch(refreshToken);
+          const { id: parentId, driveId: parentDriveId } = await getParent(fetch, name);
 
-      const url = `https://graph.microsoft.com/v1.0/drives/${driveId}/items/${id}`;
-      const response = await fetch(url, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          parentReference: {
-            id: parentId,
-            driveId: parentDriveId,
-          },
-          name: basename(name),
+          const url = `https://graph.microsoft.com/v1.0/drives/${driveId}/items/${id}`;
+          const response = await fetch(url, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              parentReference: {
+                id: parentId,
+                driveId: parentDriveId,
+              },
+              name: basename(name),
+            }),
+          });
+          const data = await response.json();
+
+          if (!response.ok) {
+            throw createError(response, data);
+          }
+
+          return formatAction('move', 'end', type, name);
         }),
-      });
-      const data = await response.json();
-
-      if (!response.ok) {
-        throw createError(response, data);
-      }
-
-      return formatAction('move', 'end', type, name);
-    }),
+      )
+    )),
   )
 );
 
